90830a8098722e540f875b22697a6405de705038
Burke Libbey <burke@burkelibbey.org>
2010-06-21 23:02:11 -0500

    lots of stuff

--- quicklaunch.coffee
+++ quicklaunch.coffee
@@ -5,20 +5,79 @@ class QuickLaunchModule
     @icon:          icon
     @relevance:     relevance
     @performAction: performAction
+  drawerLi: ->
+    "<li><img src='icons/$@icon'/>$@name</li>"
+  titleIcon: ->
+    "<img src='icons/$@icon'/>"
 
 QuickLaunch: (->
   qlform:   null
   qlinput:  null
   qldrawer: null
+  qltitleimage: null
+
+  drawerItems: ["asdf", "zxcv"]
+  drawerSelection: 0
+  drawerIsDirty: true
+
+  redrawDrawer: ->
+    if drawerIsDirty
+      qldrawer.children("li").remove()
+      for item in drawerItems
+        qldrawer.append(item.drawerLi())
+      drawerIsDirty: false
+    qldrawer.children("li").removeClass('selected')
+    qldrawer.children("li:nth($drawerSelection)").addClass('selected')
+    qltitleimage.children().remove()
+    if drawerItems.length > 0
+      qltitleimage.append(drawerItems[drawerSelection].titleIcon())
+
+  drawerDrillDown: ->
+    false
+
+  drawerDrillUp: ->
+    false
+
+  moveDrawerSelectionUp: ->
+    drawerSelection -= 1
+    drawerSelection = 0 if drawerSelection < 0
+    redrawDrawer()
+
+  moveDrawerSelectionDown: ->
+    drawerSelection += 1
+    len = drawerItems.length
+    drawerSelection = len-1 if drawerSelection > len-1
+    redrawDrawer()
+
+  qlInputDrawerEvents: (evt) ->
+    switch evt.which
+      when 37 # left
+        drawerDrillUp()
+        false
+      when 38 # up
+        moveDrawerSelectionUp()
+        redrawDrawer()
+      when 39 # right
+        drawerDrillDown()
+        false
+      when 40 # down
+        moveDrawerSelectionDown()
+        false
+      else
+        true
 
   # The timeout is necessary here for the event to run after
   # the character is inserted... Not to mention not blocking the UI.
   qlInputChange: (evt) ->
     setTimeout((->
+      drawerIsDirty: true
+      drawerSelection: 0
+      drawerItems: []
       text = qlinput.val()
       for module in modules
         if module.relevance(text) != 0
-          $("body").append("I'm relevant.")
+          drawerItems.push(module)
+      redrawDrawer()
     ), 0)
     true
 
@@ -38,20 +97,23 @@ QuickLaunch: (->
     show: ->
       qlform.fadeIn()
       qldrawer.fadeIn()
+      qltitleimage.fadeIn()
       qlinput.val("").focus()
 
     hide: ->
       qlform.fadeOut()
       qldrawer.fadeOut()
+      qltitleimage.fadeOut()
       qlinput.blur()
 
     build: ->
-      $("body").append "<div id='quicklaunchcontainer'><form id='quicklaunch'><input/></form><div id='quicklaunchdrawer'></div></div>"
+      $("body").append "<div id='quicklaunchcontainer'><div id='quicklaunchtitleimage'></div><form id='quicklaunch'><input/></form><ul id='quicklaunchdrawer'></div></div>"
       qlform = $("#quicklaunch")
       qlinput = $("#quicklaunch input")
       qldrawer = $("#quicklaunchdrawer")
-      qlinput.change qlInputChange
+      qltitleimage = $("#quicklaunchtitleimage")
       qlinput.blur this.hide
+      qlinput.keydown  qlInputDrawerEvents
       qlinput.keypress qlInputChange
       qlform.submit qlSubmit
       qlform.hide()
@@ -62,20 +124,31 @@ QuickLaunch: (->
 
 new QuickLaunchModule \
   "lookup customer by id",
-  "/images/icons/customer.png",
+  "customer.png",
   ((text) ->
     if isNaN(text)
       0
     else 1),
   ((text) -> alert("going to customer number $text"))
 
+new QuickLaunchModule \
+  "lookup product by id",
+  "product.png",
+  ((text) ->
+    if isNaN(text)
+      0
+    else 1),
+  ((text) -> alert("going to product number $text"))
+
 $ ->
   QuickLaunch.build()
   $(document).keyup (evt) ->
     switch evt.keyCode
       when 191 # /
+        return true if evt.shiftKey or evt.ctrlKey
         QuickLaunch.show()
         false
       when 27 # <esc>
         QuickLaunch.hide()
         false
+      else true
--- quicklaunch.js
+++ quicklaunch.js
@@ -8,24 +8,99 @@
     this.performAction = performAction;
     return this;
   };
+  QuickLaunchModule.prototype.drawerLi = function() {
+    return "<li><img src='icons/" + this.icon + "'/>" + this.name + "</li>";
+  };
+  QuickLaunchModule.prototype.titleIcon = function() {
+    return "<img src='icons/" + this.icon + "'/>";
+  };
 
   QuickLaunch = (function() {
-    var modules, qlInputChange, qlSubmit, qldrawer, qlform, qlinput;
+    var drawerDrillDown, drawerDrillUp, drawerIsDirty, drawerItems, drawerSelection, modules, moveDrawerSelectionDown, moveDrawerSelectionUp, qlInputChange, qlInputDrawerEvents, qlSubmit, qldrawer, qlform, qlinput, qltitleimage, redrawDrawer;
     qlform = null;
     qlinput = null;
     qldrawer = null;
+    qltitleimage = null;
+    drawerItems = ["asdf", "zxcv"];
+    drawerSelection = 0;
+    drawerIsDirty = true;
+    redrawDrawer = function() {
+      var _a, _b, _c, item;
+      if (drawerIsDirty) {
+        qldrawer.children("li").remove();
+        _b = drawerItems;
+        for (_a = 0, _c = _b.length; _a < _c; _a++) {
+          item = _b[_a];
+          qldrawer.append(item.drawerLi());
+        }
+        drawerIsDirty = false;
+      }
+      qldrawer.children("li").removeClass('selected');
+      qldrawer.children(("li:nth(" + drawerSelection + ")")).addClass('selected');
+      qltitleimage.children().remove();
+      if (drawerItems.length > 0) {
+        return qltitleimage.append(drawerItems[drawerSelection].titleIcon());
+      }
+    };
+    drawerDrillDown = function() {
+      return false;
+    };
+    drawerDrillUp = function() {
+      return false;
+    };
+    moveDrawerSelectionUp = function() {
+      drawerSelection -= 1;
+      if (drawerSelection < 0) {
+        drawerSelection = 0;
+      }
+      return redrawDrawer();
+    };
+    moveDrawerSelectionDown = function() {
+      var len;
+      drawerSelection += 1;
+      len = drawerItems.length;
+      if (drawerSelection > len - 1) {
+        drawerSelection = len - 1;
+      }
+      return redrawDrawer();
+    };
+    qlInputDrawerEvents = function(evt) {
+      var _a;
+      if ((_a = evt.which) === 37) {
+        // left
+        drawerDrillUp();
+        return false;
+      } else if (_a === 38) {
+        // up
+        moveDrawerSelectionUp();
+        return redrawDrawer();
+      } else if (_a === 39) {
+        // right
+        drawerDrillDown();
+        return false;
+      } else if (_a === 40) {
+        // down
+        moveDrawerSelectionDown();
+        return false;
+      } else {
+        return true;
+      }
+    };
     // The timeout is necessary here for the event to run after
     // the character is inserted... Not to mention not blocking the UI.
     qlInputChange = function(evt) {
       setTimeout((function() {
-        var _a, _b, _c, _d, module, text;
+        var _a, _b, _c, module, text;
+        drawerIsDirty = true;
+        drawerSelection = 0;
+        drawerItems = [];
         text = qlinput.val();
-        _a = []; _c = modules;
-        for (_b = 0, _d = _c.length; _b < _d; _b++) {
-          module = _c[_b];
-          _a.push(module.relevance(text) !== 0 ? $("body").append("I'm relevant.") : null);
+        _b = modules;
+        for (_a = 0, _c = _b.length; _a < _c; _a++) {
+          module = _b[_a];
+          module.relevance(text) !== 0 ? drawerItems.push(module) : null;
         }
-        return _a;
+        return redrawDrawer();
       }), 0);
       return true;
     };
@@ -45,20 +120,23 @@
       show: function() {
         qlform.fadeIn();
         qldrawer.fadeIn();
+        qltitleimage.fadeIn();
         return qlinput.val("").focus();
       },
       hide: function() {
         qlform.fadeOut();
         qldrawer.fadeOut();
+        qltitleimage.fadeOut();
         return qlinput.blur();
       },
       build: function() {
-        $("body").append("<div id='quicklaunchcontainer'><form id='quicklaunch'><input/></form><div id='quicklaunchdrawer'></div></div>");
+        $("body").append("<div id='quicklaunchcontainer'><div id='quicklaunchtitleimage'></div><form id='quicklaunch'><input/></form><ul id='quicklaunchdrawer'></div></div>");
         qlform = $("#quicklaunch");
         qlinput = $("#quicklaunch input");
         qldrawer = $("#quicklaunchdrawer");
-        qlinput.change(qlInputChange);
+        qltitleimage = $("#quicklaunchtitleimage");
         qlinput.blur(this.hide);
+        qlinput.keydown(qlInputDrawerEvents);
         qlinput.keypress(qlInputChange);
         qlform.submit(qlSubmit);
         qlform.hide();
@@ -66,7 +144,7 @@
       }
     };
   })();
-  new QuickLaunchModule("lookup customer by id", "/images/icons/customer.png", (function(text) {
+  new QuickLaunchModule("lookup customer by id", "customer.png", (function(text) {
     if (isNaN(text)) {
       return 0;
     } else {
@@ -75,18 +153,32 @@
   }), (function(text) {
     return alert(("going to customer number " + text));
   }));
+  new QuickLaunchModule("lookup product by id", "product.png", (function(text) {
+    if (isNaN(text)) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }), (function(text) {
+    return alert(("going to product number " + text));
+  }));
   $(function() {
     QuickLaunch.build();
     return $(document).keyup(function(evt) {
       var _a;
       if ((_a = evt.keyCode) === 191) {
         // /
+        if (evt.shiftKey || evt.ctrlKey) {
+          return true;
+        }
         QuickLaunch.show();
-        return false;
+        false;
       } else if (_a === 27) {
         // <esc>
         QuickLaunch.hide();
-        return false;
+        false;
+      } else {
+        true;
       }
     });
   });
