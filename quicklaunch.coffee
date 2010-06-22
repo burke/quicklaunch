c25b5c9e495f9346b80d0e577159c057c10d9341
Burke Libbey <burke@burkelibbey.org>
2010-06-21 21:18:40 -0500

    more awesomeness

--- quicklaunch.coffee
+++ quicklaunch.coffee
@@ -1,40 +1,80 @@
-QuickLaunch: (->
-  qlform: null
-  qlinput: null
+class QuickLaunchModule
+  constructor: (name, icon, relevance, performAction) ->
+    QuickLaunch.registerModule(this)
+    @name:          name
+    @icon:          icon
+    @relevance:     relevance
+    @performAction: performAction
 
+QuickLaunch: (->
+  qlform:   null
+  qlinput:  null
+  qldrawer: null
+
+  # The timeout is necessary here for the event to run after
+  # the character is inserted... Not to mention not blocking the UI.
   qlInputChange: (evt) ->
+    setTimeout((->
+      text = qlinput.val()
+      for module in modules
+        if module.relevance(text) != 0
+          $("body").append("I'm relevant.")
+    ), 0)
     true
 
   qlSubmit: (evt) ->
-    false
+    modules[0].performAction(qlinput.val())
+    true
+
+  modules: []
 
   return { # public ###########################################################
-    trigger: ->
-      qlform.show()
-      qlinput.focus()
+    registerModule: (module) ->
+      modules.push(module)
+
+    showDrawer: ->
+      qldrawer.fadeIn()
+
+    show: ->
+      qlform.fadeIn()
+      qldrawer.fadeIn()
+      qlinput.val("").focus()
 
     hide: ->
-      qlform.hide()
-      qlinput.blur().val("")
+      qlform.fadeOut()
+      qldrawer.fadeOut()
+      qlinput.blur()
 
     build: ->
-      $("body").append "<form id='quicklaunch'><input/></form>"
+      $("body").append "<div id='quicklaunchcontainer'><form id='quicklaunch'><input/></form><div id='quicklaunchdrawer'></div></div>"
       qlform = $("#quicklaunch")
       qlinput = $("#quicklaunch input")
+      qldrawer = $("#quicklaunchdrawer")
       qlinput.change qlInputChange
       qlinput.blur this.hide
       qlinput.keypress qlInputChange
       qlform.submit qlSubmit
       qlform.hide()
+      qldrawer.hide()
   }
 )()
+
+
+new QuickLaunchModule \
+  "lookup customer by id",
+  "/images/icons/customer.png",
+  ((text) ->
+    if isNaN(text)
+      0
+    else 1),
+  ((text) -> alert("going to customer number $text"))
 
 $ ->
   QuickLaunch.build()
   $(document).keyup (evt) ->
     switch evt.keyCode
       when 191 # /
-        QuickLaunch.trigger()
+        QuickLaunch.show()
         false
       when 27 # <esc>
         QuickLaunch.hide()
--- quicklaunch.js
+++ quicklaunch.js
@@ -1,44 +1,87 @@
 (function(){
-  var QuickLaunch;
+  var QuickLaunch, QuickLaunchModule;
+  QuickLaunchModule = function(name, icon, relevance, performAction) {
+    QuickLaunch.registerModule(this);
+    this.name = name;
+    this.icon = icon;
+    this.relevance = relevance;
+    this.performAction = performAction;
+    return this;
+  };
+
   QuickLaunch = (function() {
-    var qlInputChange, qlSubmit, qlform, qlinput;
+    var modules, qlInputChange, qlSubmit, qldrawer, qlform, qlinput;
     qlform = null;
     qlinput = null;
+    qldrawer = null;
+    // The timeout is necessary here for the event to run after
+    // the character is inserted... Not to mention not blocking the UI.
     qlInputChange = function(evt) {
+      setTimeout((function() {
+        var _a, _b, _c, _d, module, text;
+        text = qlinput.val();
+        _a = []; _c = modules;
+        for (_b = 0, _d = _c.length; _b < _d; _b++) {
+          module = _c[_b];
+          _a.push(module.relevance(text) !== 0 ? $("body").append("I'm relevant.") : null);
+        }
+        return _a;
+      }), 0);
       return true;
     };
     qlSubmit = function(evt) {
-      return false;
+      modules[0].performAction(qlinput.val());
+      return true;
     };
+    modules = [];
     return {
       // public ###########################################################
-      trigger: function() {
-        qlform.show();
-        return qlinput.focus();
+      registerModule: function(module) {
+        return modules.push(module);
+      },
+      showDrawer: function() {
+        return qldrawer.fadeIn();
+      },
+      show: function() {
+        qlform.fadeIn();
+        qldrawer.fadeIn();
+        return qlinput.val("").focus();
       },
       hide: function() {
-        qlform.hide();
-        return qlinput.blur().val("");
+        qlform.fadeOut();
+        qldrawer.fadeOut();
+        return qlinput.blur();
       },
       build: function() {
-        $("body").append("<form id='quicklaunch'><input/></form>");
+        $("body").append("<div id='quicklaunchcontainer'><form id='quicklaunch'><input/></form><div id='quicklaunchdrawer'></div></div>");
         qlform = $("#quicklaunch");
         qlinput = $("#quicklaunch input");
+        qldrawer = $("#quicklaunchdrawer");
         qlinput.change(qlInputChange);
         qlinput.blur(this.hide);
         qlinput.keypress(qlInputChange);
         qlform.submit(qlSubmit);
-        return qlform.hide();
+        qlform.hide();
+        return qldrawer.hide();
       }
     };
   })();
+  new QuickLaunchModule("lookup customer by id", "/images/icons/customer.png", (function(text) {
+    if (isNaN(text)) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }), (function(text) {
+    return alert(("going to customer number " + text));
+  }));
   $(function() {
     QuickLaunch.build();
     return $(document).keyup(function(evt) {
       var _a;
       if ((_a = evt.keyCode) === 191) {
         // /
-        QuickLaunch.trigger();
+        QuickLaunch.show();
         return false;
       } else if (_a === 27) {
         // <esc>
